1f6461623402765a66ca6be43fa0c8f5cabc9a2f
văn sĩ <vansi19a@gmail.com>
2021-10-04 09:18:45 +0700

    Add project files.

---
+++ CRUDApiMicrosoft/Controllers/ApiDataController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using CRUDApiMicrosoft.Models;
+
+namespace CRUDApiMicrosoft.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiDataController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ApiDataController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string sql = @"
+                select OrderId, DateTime, ClientCode, StockCode, MarketSegment, BuySell, OrderType, RemainingQty, Quantity, Price, Exchange, Status, TradingAccount, Message from 
+                dbo.tbl_OrderHistory";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("Db_EzTrade4-clone");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(sql, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpPost("SendOrder")]
+        public JsonResult Post(OrderHistory OH)
+        {
+            string sql = $"insert into dbo.tbl_OrderHistory (DateTime, ClientCode, StockCode, MarketSegment, BuySell, OrderType, RemainingQty, Quantity, Price, Exchange, Status, TradingAccount, Message) values ('{DateTime.Now}', '{OH.ClientCode}', '{OH.StockCode}', '{OH.MarketSegment}', '{OH.BuySell}', '{OH.OrderType}', '{OH.RemainingQty.ToString()}', '{OH.Quantity.ToString()}', '{OH.Price.ToString()}', '{OH.Exchange}', '{OH.Status}', '{OH.TradingAccount}', '{OH.Message}')";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("Db_EzTrade4-clone");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(sql, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("DateTime", OH.DateTime = DateTime.Now);
+                    myCommand.Parameters.AddWithValue("ClientCode", OH.ClientCode);
+                    myCommand.Parameters.AddWithValue("StockCode", OH.StockCode);
+                    myCommand.Parameters.AddWithValue("MarketSegment", OH.MarketSegment);
+                    myCommand.Parameters.AddWithValue("BuySell", OH.BuySell);
+                    myCommand.Parameters.AddWithValue("OrderType", OH.OrderType);
+                    myCommand.Parameters.AddWithValue("RemainingQty", OH.RemainingQty);
+                    myCommand.Parameters.AddWithValue("Quantity", OH.Quantity);
+                    myCommand.Parameters.AddWithValue("Price", OH.Price);
+                    myCommand.Parameters.AddWithValue("Exchange", OH.Exchange);
+                    myCommand.Parameters.AddWithValue("Status", OH.Status);
+                    myCommand.Parameters.AddWithValue("TradingAccount", OH.TradingAccount);
+                    myCommand.Parameters.AddWithValue("Message", OH.Message);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("them thanh cong");
+        }
+        [HttpPut("{OrderId}")]
+        public JsonResult Put(int OrderId, OrderHistory OH)
+        {
+            string sql = @"update dbo.tbl_OrderHistory set DateTime =@DateTime, ClientCode=@ClientCode, StockCode=@StockCode, MarketSegment=@MarketSegment, BuySell=@BuySell, OrderType=@OrderType, RemainingQty=@RemainingQty, Quantity=@Quantity, Price=@Price, Exchange=@Exchange, Status=@Status, TradingAccount=@TradingAccount, Message=@Message where OrderId=@OrderId";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("Db_EzTrade4-clone");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(sql, myCon))
+                {
+
+                    myCommand.Parameters.AddWithValue("@OrderId", OrderId);
+                    myCommand.Parameters.AddWithValue("@DateTime", OH.DateTime = DateTime.Now);
+                    myCommand.Parameters.AddWithValue("@ClientCode", OH.ClientCode);
+                    myCommand.Parameters.AddWithValue("@StockCode", OH.StockCode);
+                    myCommand.Parameters.AddWithValue("@MarketSegment", OH.MarketSegment);
+                    myCommand.Parameters.AddWithValue("@BuySell", OH.BuySell);
+                    myCommand.Parameters.AddWithValue("@OrderType", OH.OrderType);
+                    myCommand.Parameters.AddWithValue("@RemainingQty", OH.RemainingQty.ToString());
+                    myCommand.Parameters.AddWithValue("@Quantity", OH.Quantity.ToString());
+                    myCommand.Parameters.AddWithValue("@Price", OH.Price.ToString());
+                    myCommand.Parameters.AddWithValue("@Exchange", OH.Exchange);
+                    myCommand.Parameters.AddWithValue("@Status", OH.Status);
+                    myCommand.Parameters.AddWithValue("@TradingAccount", OH.TradingAccount);
+                    myCommand.Parameters.AddWithValue("@Message", OH.Message);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("update thanh cong");
+        }
+        [HttpDelete("{OrderId}")]
+        public JsonResult Delete(int OrderId)
+        {
+            string sql = @"delete from dbo.tbl_OrderHistory where OrderId=@OrderId";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("Db_EzTrade4-clone");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(sql, myCon))
+                {
+           
+                    myCommand.Parameters.AddWithValue("OrderId", OrderId);
+                
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("xoa thanh cong");
+        }
+    }
+}
---
+++ CRUDApiMicrosoft/Models/OrderHistory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDApiMicrosoft.Models
+{
+    public class OrderHistory
+    {
+        [Column(TypeName = "DateTime")]
+        public DateTime DateTime { get; set; }
+    
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column(TypeName = "int")]
+        public int OrderId { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string ClientCode { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string StockCode { get; set; }
+      
+        [Column(TypeName = "nvarchar(20)")]
+        public string MarketSegment { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string TradingAccount { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string Status { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string Exchange { get; set; }
+  
+        [Column(TypeName = "int")]
+        public int Price { get; set; }
+
+
+        [Column(TypeName = "int")]
+        public int Quantity { get; set; }
+
+
+        [Column(TypeName = "int")]
+        public int RemainingQty { get; set; }
+   
+        [Column(TypeName = "nvarchar(20)")]
+        public string OrderType { get; set; }
+
+ 
+        [Column(TypeName = "nvarchar(20)")]
+        public string BuySell { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Message { get; set; }
+    }
+}
